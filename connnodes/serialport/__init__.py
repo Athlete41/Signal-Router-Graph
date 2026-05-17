46521478319b8f69232b88630ebefe6e0b9c6003
Athlete41 <2322012547@qq.com>
2026-05-17 20:54:21 +0800

    添加串口参数保存功能

--- connnodes/serialport/__init__.py
+++ connnodes/serialport/__init__.py
@@ -4,6 +4,7 @@ from .serialport_widget import SerialPort_Widget
 from conn_conf import register_node
 from conn_base import ConnNode, ConnSocketConf
 from PyQt5.QtCore import QByteArray
+from PyQt5.QtSerialPort import QSerialPort
 
 
 @register_node()
@@ -50,6 +51,8 @@ class SerialPortNode(ConnNode):
         self.registerSignal("received", self.content.dataSource.received)
         self.registerSlot("sendData", self.content.dataSource.sendData)
         self.registerSlot("sendText", self.content.dataSource.sendText)
+
+        self.content: SerialPort_Widget
    
 
     def initInnerClasses(self):
@@ -64,3 +67,36 @@ class SerialPortNode(ConnNode):
         self.input_multi_edged = True 
         self.input_socket_position = LEFT_BOTTOM
         self.output_socket_position = RIGHT_BOTTOM
+
+
+    def serialize(self):
+        res = super().serialize()
+        device = self.content.dataSource.device
+
+        res["baud_rate"] = device.baudRate()
+        res["data_bits"] = device.dataBits()
+        res["parity"] = device.parity()
+        res["stop_bits"] = device.stopBits()
+        res["flow_control"] = device.flowControl()
+
+        res["info_log_enable"] = self.content.ui.infoLogBtn.isChecked()
+        res["warning_log_enable"] = self.content.ui.warningLogBtn.isChecked()
+        res["error_log_enable"] = self.content.ui.errorLogBtn.isChecked()
+        
+        return res
+
+    def deserialize(self, data, hashmap={}, restore_id=True):
+        res = super().deserialize(data, hashmap, restore_id)
+
+        device = self.content.dataSource.device
+        device.setBaudRate(data.get("baud_rate", 115200))
+        device.setDataBits(data.get("data_bits", 8))
+        device.setParity(data.get("parity", QSerialPort.NoParity))
+        device.setStopBits(data.get("stop_bits", QSerialPort.OneStop))
+        device.setFlowControl(data.get("flow_control", QSerialPort.NoFlowControl))
+
+        self.content.ui.infoLogBtn.setChecked(data.get("info_log_enable", False))
+        self.content.ui.warningLogBtn.setChecked(data.get("warning_log_enable", False))
+        self.content.ui.errorLogBtn.setChecked(data.get("error_log_enable", False))
+
+        return res
