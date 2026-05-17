a921ccbea05ffa5c3875dfcb88c8d5a29a0bc795
Athlete41 <2322012547@qq.com>
2026-05-17 21:13:14 +0800

    合并 data_sender_node.py

--- connnodes/data_sender_node.py
+++ connnodes/data_sender_node.py
@@ -1,5 +1,7 @@
-from qtpy.QtWidgets import QTextEdit, QHBoxLayout, QVBoxLayout, QPushButton, QCheckBox
-from PyQt5.QtCore import pyqtSignal, QByteArray
+from qtpy.QtWidgets import QTextEdit, QVBoxLayout, QPushButton, QCheckBox, QDoubleSpinBox
+from qtpy.QtCore import Qt
+
+from PyQt5.QtCore import pyqtSignal, QByteArray, QTimer
 from nodeeditor.node_content_widget import QDMNodeContentWidget
 
 from conn_conf import register_node
@@ -8,7 +10,7 @@ from conn_utils import easyError
 
 
 
-class DataSenderContent(QDMNodeContentWidget):
+class DataSenderAdvancedContent(QDMNodeContentWidget):
     sendDataNotify = pyqtSignal(QByteArray)
     sendTextNotify = pyqtSignal(str)
 
@@ -17,15 +19,29 @@ class DataSenderContent(QDMNodeContentWidget):
         self.textEdit = QTextEdit(self)
         self.autoLineBreak = QCheckBox(self)
         self.sendBtn = QPushButton("发送", self)
+        self.timer = QTimer(self)
+        self.timerEnable = QCheckBox(self)
+        self.timerDurationSetter = QDoubleSpinBox(self)
+
 
         self.setLayout(layout)
         layout.addWidget(self.textEdit)
         layout.addWidget(self.autoLineBreak)
+        layout.addWidget(self.timerDurationSetter)
+        layout.addWidget(self.timerEnable)
         layout.addWidget(self.sendBtn)
-
+        
+        self.timerEnable.setText("启用定时器")
         self.autoLineBreak.setText("自动新行")
         self.autoLineBreak.setChecked(True)
         self.sendBtn.clicked.connect(self.sendData)
+        self.timerEnable.stateChanged.connect(self.timerEnableChangedHandler)
+        self.timer.timeout.connect(self.sendData)
+        self.timerDurationSetter.valueChanged.connect(self.timerDurationChangedHandler)
+        self.timerDurationSetter.setMinimum(0.01)
+        self.timerDurationSetter.setDecimals(3)
+        self.timerDurationSetter.setValue(1)
+
 
         # TODO 暂时没找到细致修改全局样式的方法, 这里先简单处理
         self.setStyleSheet("""                 
@@ -34,6 +50,15 @@ QCheckBox {
 }
 """)
 
+    def timerEnableChangedHandler(self, state: int):
+        if state == Qt.Checked:
+            self.timer.start(int(max(self.timerDurationSetter.value(), 0.01) * 1000))
+        else:
+            self.timer.stop()
+
+    def timerDurationChangedHandler(self, value: float):
+        self.timer.stop()
+        if self.timerEnable.isChecked(): self.timer.start(int(max(value, 0.01) * 1000))
 
     def sendData(self):
         text = self.textEdit.toPlainText() + ("\n" if self.autoLineBreak.isChecked() else "")
@@ -46,14 +71,14 @@ QCheckBox {
         ...
 
 @register_node()
-class DataSenderNode(ConnNode):
-    tppath = ("数据源", "数据发送器")
+class DataSenderAdvancedNode(ConnNode):
+    tppath = ("数据源", "发送器")
     icon = "icons/emitter.png"
-    name = "数据发送器"
-    tooltip = "可以发送 QByteArray 类型数据"
-    conn_title = "数据发送器"
+    name = "发送器"
+    tooltip = "可以发送 QByteArray 类型数据, 支持定时器发送"
+    conn_title = "发送器"
 
-    NodeContent_class = DataSenderContent
+    NodeContent_class = DataSenderAdvancedContent
 
     def __init__(self, scene):
         super().__init__(scene,
@@ -82,7 +107,7 @@ class DataSenderNode(ConnNode):
 
     def initInnerClasses(self):
         super().initInnerClasses()
-        self.grNode.height = 180 # 设置高度
+        self.grNode.height = 230
         self.grNode.width = 180
 
 
@@ -90,6 +115,8 @@ class DataSenderNode(ConnNode):
         res = super().serialize()
         res['text_content'] = self.content.textEdit.toPlainText()
         res['auto_line_break_enabled'] = self.content.autoLineBreak.isChecked()
+        res['timer_enabled'] = self.content.timerEnable.isChecked()
+        res['timer_duration'] = self.content.timerDurationSetter.value()
         
         return res
 
@@ -98,6 +125,8 @@ class DataSenderNode(ConnNode):
 
         self.content.textEdit.setPlainText(data.get('text_content', ""))
         self.content.autoLineBreak.setChecked(data.get('auto_line_break_enabled', True))
+        self.content.timerEnable.setChecked(data.get('timer_enabled', False))
+        self.content.timerDurationSetter.setValue(data.get('timer_duration', 1.0))
 
         return res
     
